bb86ead5af6d26e23a263f6d78fe46e713012d5a
Ralf Horstmann <ralf+github@ackstorm.de>
2017-01-05 11:40:45 +0100

    Whitespace

--- eppformat.py
+++ eppformat.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-# 
+#
 # Copyright (c) 2017 Ralf Horstmann <ralf@ackstorm.de>
-# 
+#
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 # WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 # MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -204,7 +204,7 @@ epp_file = Struct(
             "data" / Array(this.header.length, Struct(
                 "val1"       / Int32ul,
                 "val2"       / Float32l,
-                "val3"       / Float32l,  
+                "val3"       / Float32l,
             )),
         ),
     })),
