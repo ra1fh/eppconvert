88b6cf45bae64e8591be159ddb3d151d79d97b17
Ralf Horstmann <ralf+github@ackstorm.de>
2017-01-22 22:32:30 +0100

    Split worker function that operates on strings

--- eppconvert/eppread.py
+++ eppconvert/eppread.py
@@ -20,27 +20,43 @@ parse daum ergo bike epp/eup files
 
 from __future__ import print_function
 
+import os
 import sys
+import string
 import eppconvert.eppformat as epp
 
-def main():
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+def eppread(document, limit=None):
+    output = StringIO()
+    doc = epp.epp_file.parse(document)
+    output.write("signature = {0}\n".format(doc.signature.decode('ascii')))
+    output.write("version = {0}\n".format(doc.version))
+    output.write(str(doc.header) + "\n")
+    output.write("\n".join(map(str, doc.data[:limit])))
+    return output.getvalue()
+
+def main(argv=None):
+    if (argv == None):
+        argv = sys.argv
     if (sys.version_info < (3,0)):
         reload(sys)
         sys.setdefaultencoding('utf-8')
-    if (len(sys.argv) > 1):
-        p = epp.epp_file.parse_stream(open(sys.argv[1], "rb"))
-        print("signature =", p.signature.decode('ascii'))
-        print("version =", p.version)
-        print(p.header)
-        if (len(sys.argv) > 2):
-            limit=int(sys.argv[2])
+    if (len(argv) > 1):
+        if (len(argv) > 2):
+            limit=int(argv[2])
         else:
             limit=None
-        for v in p.data[:limit]:
-            print(v)
+        with open(argv[1], 'rb') as f:
+            text = eppread(f.read(), limit)
+            print(text)
+        return 0
     else:
         print("usage: eppread <file> [limit]")
-        sys.exit(1);
+        return 1
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
