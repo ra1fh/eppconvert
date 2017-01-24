6eeb833eb76b4fa3bd0ab83c7ada1371c9aeac30
Ralf Horstmann <ralf+github@ackstorm.de>
2017-01-24 19:54:16 +0100

    Add more test cases for stdout cases

--- test/test_eppconvert.py
+++ test/test_eppconvert.py
@@ -12,10 +12,6 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-import os
-import codecs
-import pytest
-import filecmp
 import difflib
 import filecmp
 import hexdump
@@ -35,19 +31,14 @@ def testfile(request):
     testfile = request.param
     yield testfile
 
-def test_eppread(testfile):
-    basename = 'test' + os.sep + testfile
-    result = eppread.main(['-i', basename + '.epp', '-o', basename + '.test.txt'])
-    assert(result == 0)
-    a = codecs.open(basename + '.test.txt', encoding='utf-8').readlines()
-    b = codecs.open(basename + '.txt', encoding='utf-8').readlines()
-    print(''.join(difflib.unified_diff(a,b, basename + '.test.txt', basename + '.txt')))
-    assert(filecmp.cmp(basename + '.txt', basename + '.test.txt'))
-
 def test_gpx2epp(testfile):
+    ''' test conversion from gpx to epp '''
     basename = 'test' + os.sep + testfile
-    result = gpx2epp.main(['-i', basename + '.gpx', '-o', basename + '.test.epp'])
+    iname = basename + '.gpx'
+    oname = basename + '.test.epp'
+    result = gpx2epp.main(['-i', iname, '-o', oname, '-s', '200'])
     assert(result == 0)
+    # generate diff of hexdumps that will be shown in case of failure
     with open(basename + '.test.epp', 'rb') as infile:
         hd = hexdump.hexdump(infile.read(), result='return')
         with open(basename + '.test.hex', 'w') as outfile:
@@ -56,6 +47,40 @@ def test_gpx2epp(testfile):
     b = open(basename + '.hex', 'r').readlines()
     print(''.join(difflib.unified_diff(a,b, basename + '.test.hex', basename + '.hex')))
     assert(filecmp.cmp(basename + '.hex', basename + '.test.hex'))
+
+def test_gpx2epp_stdout(testfile, capfd):
+    ''' test conversion from epp to plain text on stdout'''
+    basename = 'test' + os.sep + testfile
+    iname = basename + '.gpx'
+    result = gpx2epp.main(['-i', iname])
+    assert(result == 0)
+    b = io.open(basename + '.epp', 'rb').read()
+    out, err = capfd.readouterr()
+    assert(out.startswith('EW2_EUP'))
+
+def test_eppread(testfile):
+    ''' test conversion from epp to plain text '''
+    basename = 'test' + os.sep + testfile
+    iname = basename + '.epp'
+    oname = basename + '.test.txt'
+    result = eppread.main(['-i', iname, '-o', oname])
+    assert(result == 0)
+    # generate diff that will be shown in case of failure
+    a = io.open(basename + '.test.txt', encoding='utf-8').readlines()
+    b = io.open(basename + '.txt', encoding='utf-8').readlines()
+    print(''.join(difflib.unified_diff(a,b, basename + '.test.txt', basename + '.txt')))
+    assert(filecmp.cmp(basename + '.txt', basename + '.test.txt'))
+
+def test_eppread_stdout(testfile, capfd):
+    ''' test conversion from epp to plain text on stdout'''
+    basename = 'test' + os.sep + testfile
+    iname = basename + '.epp'
+    result = eppread.main(['-i', iname])
+    assert(result == 0)
+    out, err = capfd.readouterr()
+    b = io.open(basename + '.txt', encoding='utf-8').read()
+    assert(out == b)
+
 if __name__ == "__main__":
     ''' setup testcases data '''
     print("Generating known good data for test cases.")
